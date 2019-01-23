3c62e400f492cbb24cc6704a9f982b879272705d
Zach Vida <vida.zach@gmail.com>
2019-01-23 10:58:48 -0500

    fix metrics server test'

--- spec/classes/gitlab_ci_runner_spec.rb
+++ spec/classes/gitlab_ci_runner_spec.rb
@@ -63,7 +63,7 @@ describe 'gitlab_ci_runner', type: :class do
         it { is_expected.to contain_file_line('gitlab-runner-metrics-server').that_requires("Package[#{package_name}]") }
         it { is_expected.to contain_file_line('gitlab-runner-metrics-server').that_notifies('Exec[gitlab-runner-restart]') }
         it do
-          is_expected.to contain_file_line('gitlab-runner-builds_dir').with('path' => '/etc/gitlab-runner/config.toml',
+          is_expected.to contain_file_line('gitlab-runner-metrics-server').with('path' => '/etc/gitlab-runner/config.toml',
                                                                                 'line'  => 'metrics_server  = "localhost:9252"',
                                                                                 'match' => '^metrics_server = .+')
         end
