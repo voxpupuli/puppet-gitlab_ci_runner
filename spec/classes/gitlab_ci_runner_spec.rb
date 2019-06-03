e2b099ccbb0263429d81a549b219a5dfd942985d
tipmg <duergar1986@gmail.com>
2019-06-03 18:42:22 +0300

    update test

--- spec/classes/gitlab_ci_runner_spec.rb
+++ spec/classes/gitlab_ci_runner_spec.rb
@@ -21,20 +21,20 @@ describe 'gitlab_ci_runner', type: :class do
         }
       end
 
-      it {is_expected.to compile.with_all_deps}
+      it { is_expected.to compile.with_all_deps }
 
-      it {is_expected.to contain_class('docker::images')}
-      it {is_expected.to contain_package('gitlab-runner')}
-      it {is_expected.to contain_exec('gitlab-runner-restart').that_requires("Package[#{package_name}]")}
+      it { is_expected.to contain_class('docker::images') }
+      it { is_expected.to contain_package('gitlab-runner') }
+      it { is_expected.to contain_exec('gitlab-runner-restart').that_requires("Package[#{package_name}]") }
       it do
         is_expected.to contain_exec('gitlab-runner-restart').with('command' => "/usr/bin/#{package_name} restart",
                                                                   'refreshonly' => true)
       end
-      it {is_expected.to contain_gitlab_ci_runner__runner('test_runner').that_requires('Exec[gitlab-runner-restart]')}
-      it {is_expected.not_to contain_file_line('gitlab-runner-concurrent')}
-      it {is_expected.not_to contain_file_line('gitlab-runner-listen-address')}
-      it {is_expected.not_to contain_file_line('gitlab-runner-builds_dir')}
-      it {is_expected.not_to contain_file_line('gitlab-runner-cache_dir')}
+      it { is_expected.to contain_gitlab_ci_runner__runner('test_runner').that_requires('Exec[gitlab-runner-restart]') }
+      it { is_expected.not_to contain_file_line('gitlab-runner-concurrent') }
+      it { is_expected.not_to contain_file_line('gitlab-runner-listen-address') }
+      it { is_expected.not_to contain_file_line('gitlab-runner-builds_dir') }
+      it { is_expected.not_to contain_file_line('gitlab-runner-cache_dir') }
 
       context 'with concurrent => 10' do
         let(:params) do
@@ -45,8 +45,8 @@ describe 'gitlab_ci_runner', type: :class do
           }
         end
 
-        it {is_expected.to contain_file_line('gitlab-runner-concurrent').that_requires("Package[#{package_name}]")}
-        it {is_expected.to contain_file_line('gitlab-runner-concurrent').that_notifies('Exec[gitlab-runner-restart]')}
+        it { is_expected.to contain_file_line('gitlab-runner-concurrent').that_requires("Package[#{package_name}]") }
+        it { is_expected.to contain_file_line('gitlab-runner-concurrent').that_notifies('Exec[gitlab-runner-restart]') }
         it do
           is_expected.to contain_file_line('gitlab-runner-concurrent').with('path' => '/etc/gitlab-runner/config.toml',
                                                                             'line' => 'concurrent = 10',
@@ -62,8 +62,8 @@ describe 'gitlab_ci_runner', type: :class do
           }
         end
 
-        it {is_expected.to contain_file_line('gitlab-runner-listen-address').that_requires("Package[#{package_name}]")}
-        it {is_expected.to contain_file_line('gitlab-runner-listen-address').that_notifies('Exec[gitlab-runner-restart]')}
+        it { is_expected.to contain_file_line('gitlab-runner-listen-address').that_requires("Package[#{package_name}]") }
+        it { is_expected.to contain_file_line('gitlab-runner-listen-address').that_notifies('Exec[gitlab-runner-restart]') }
 
 
         it do
@@ -81,8 +81,8 @@ describe 'gitlab_ci_runner', type: :class do
           }
         end
 
-        it {is_expected.to contain_file_line('gitlab-runner-builds_dir').that_requires("Package[#{package_name}]")}
-        it {is_expected.to contain_file_line('gitlab-runner-builds_dir').that_notifies('Exec[gitlab-runner-restart]')}
+        it { is_expected.to contain_file_line('gitlab-runner-builds_dir').that_requires("Package[#{package_name}]") }
+        it { is_expected.to contain_file_line('gitlab-runner-builds_dir').that_notifies('Exec[gitlab-runner-restart]') }
         it do
           is_expected.to contain_file_line('gitlab-runner-builds_dir').with('path' => '/etc/gitlab-runner/config.toml',
                                                                             'line' => 'builds_dir = "/tmp/builds_dir"',
@@ -98,8 +98,8 @@ describe 'gitlab_ci_runner', type: :class do
           }
         end
 
-        it {is_expected.to contain_file_line('gitlab-runner-cache_dir').that_requires("Package[#{package_name}]")}
-        it {is_expected.to contain_file_line('gitlab-runner-cache_dir').that_notifies('Exec[gitlab-runner-restart]')}
+        it { is_expected.to contain_file_line('gitlab-runner-cache_dir').that_requires("Package[#{package_name}]") }
+        it { is_expected.to contain_file_line('gitlab-runner-cache_dir').that_notifies('Exec[gitlab-runner-restart]') }
         it do
           is_expected.to contain_file_line('gitlab-runner-cache_dir').with('path' => '/etc/gitlab-runner/config.toml',
                                                                            'line' => 'cache_dir = "/tmp/cache_dir"',
@@ -115,8 +115,8 @@ describe 'gitlab_ci_runner', type: :class do
           }
         end
 
-        it {is_expected.to contain_file_line('gitlab-runner-sentry_dsn').that_requires("Package[#{package_name}]")}
-        it {is_expected.to contain_file_line('gitlab-runner-sentry_dsn').that_notifies('Exec[gitlab-runner-restart]')}
+        it { is_expected.to contain_file_line('gitlab-runner-sentry_dsn').that_requires("Package[#{package_name}]") }
+        it { is_expected.to contain_file_line('gitlab-runner-sentry_dsn').that_notifies('Exec[gitlab-runner-restart]') }
         it do
           is_expected.to contain_file_line('gitlab-runner-sentry_dsn').with('path' => '/etc/gitlab-runner/config.toml',
                                                                             'line' => 'sentry_dsn = "https://123abc@localhost/1"',
@@ -134,8 +134,8 @@ describe 'gitlab_ci_runner', type: :class do
           )
         end
 
-        it {is_expected.to contain_exec('Register_runner_test_runner').with('command' => %r{/usr/bin/[^ ]+ register })}
-        it {is_expected.not_to contain_exec('Register_runner_test_runner').with('command' => %r{--ensure=})}
+        it { is_expected.to contain_exec('Register_runner_test_runner').with('command' => %r{/usr/bin/[^ ]+ register }) }
+        it { is_expected.not_to contain_exec('Register_runner_test_runner').with('command' => %r{--ensure=}) }
       end
 
       context 'with ensure => absent' do
@@ -149,8 +149,8 @@ describe 'gitlab_ci_runner', type: :class do
           )
         end
 
-        it {is_expected.to contain_exec('Unregister_runner_test_runner').with('command' => %r{/usr/bin/[^ ]+ unregister })}
-        it {is_expected.not_to contain_exec('Unregister_runner_test_runner').with('command' => %r{--ensure=})}
+        it { is_expected.to contain_exec('Unregister_runner_test_runner').with('command' => %r{/usr/bin/[^ ]+ unregister }) }
+        it { is_expected.not_to contain_exec('Unregister_runner_test_runner').with('command' => %r{--ensure=}) }
       end
     end
   end
