9a3ecfa47f21fedc573391f14950b9e63cdb7985
Matthias Baur <m.baur@syseleven.de>
2021-07-15 15:47:35 +0200

    Quote the API response

--- spec/acceptance/gitlab_ci_runner_spec.rb
+++ spec/acceptance/gitlab_ci_runner_spec.rb
@@ -39,7 +39,7 @@ describe 'gitlab_ci_runner class' do
     it 'registered the runner' do
       authtoken = shell("grep 'token = ' /etc/gitlab-runner/config.toml | cut -d '\"' -f2").stdout
       shell("/usr/bin/env curl -X POST --form 'token=#{authtoken}' http://gitlab/api/v4/runners/verify") do |r|
-        expect(r.stdout).to eq('200')
+        expect(r.stdout).to eq('"200"')
       end
     end
   end
