327032ac41e218a155f88be1854db221df30eec6
Romain Tartière <romain@blogreen.org>
2021-09-01 20:34:41 -1000

    Fix documentation generation

--- lib/puppet/functions/gitlab_ci_runner/to_toml.rb
+++ lib/puppet/functions/gitlab_ci_runner/to_toml.rb
@@ -1,15 +1,13 @@
 require_relative '../../../puppet_x/gitlab/dumper.rb'
 # @summary Convert a data structure and output to TOML.
-#
-# @example How to output TOML to a file
-#   file { '/tmp/config.toml':
-#     ensure  => file,
-#     content => to_toml($myhash),
-#   }
-#
 Puppet::Functions.create_function(:'gitlab_ci_runner::to_toml') do
   # @param data Data structure which needs to be converted into TOML
   # @return [String] Converted data as TOML string
+  # @example How to output TOML to a file
+  #     file { '/tmp/config.toml':
+  #       ensure  => file,
+  #       content => to_toml($myhash),
+  #     }
   dispatch :to_toml do
     required_param 'Hash', :data
     return_type 'String'
