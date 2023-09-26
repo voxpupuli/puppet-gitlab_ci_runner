9b2ada9899e139fd7e1341a7086ef3c9ba14954b
Romain Tartière <romain@blogreen.org>
2023-09-25 15:12:01 -1000

    Fix CI with Ruby 3.2

--- spec/tasks/unregister_runner_spec.rb
+++ spec/tasks/unregister_runner_spec.rb
@@ -15,17 +15,17 @@ describe UnregisterRunnerTask do
   describe '.task' do
     it 'can unregister a runner' do
       allow(PuppetX::Gitlab::Runner).to receive(:unregister).
-        with('https://gitlab.example.org', token: 'abcdef1234').
+        with('https://gitlab.example.org', { token: 'abcdef1234' }).
         and_return({})
 
-      expect(described_object.task(params)).to eq(status: 'success')
+      expect(described_object.task(**params)).to eq(status: 'success')
     end
 
     it 'can raise an error' do
       params.merge(token: 'invalid-token')
       stub_request(:delete, 'https://gitlab.example.org/api/v4/runners').
         to_return(status: [403, 'Forbidden'])
-      expect { described_object.task(params) }.to raise_error(TaskHelper::Error, %r{Gitlab runner failed to unregister: Forbidden})
+      expect { described_object.task(**params) }.to raise_error(TaskHelper::Error, %r{Gitlab runner failed to unregister: Forbidden})
     end
   end
 end
