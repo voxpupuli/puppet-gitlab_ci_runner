398677e132cada543907da5d858997770fd15f7b
Steve Traylen <steve.traylen@cern.ch>
2024-11-21 13:23:41 +0100

    Respect bolt_supported from beaker_helper
    Disable beaker tests based on the bolt supported setting
    from beaker_helper.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,5 +6,6 @@ ENV['BEAKER_FACTER_GITLAB_IP'] = File.read(File.expand_path('~/GITLAB_IP')).chom
 ENV['BEAKER_FACTER_SQUID_IP'] = File.read(File.expand_path('~/SQUID_IP')).chomp
 
 configure_beaker do |host|
+  host.install_package('puppet-bolt') if bolt_supported?(host)
   install_puppet_module_via_pmt_on(host, 'puppetlabs/docker')
 end
